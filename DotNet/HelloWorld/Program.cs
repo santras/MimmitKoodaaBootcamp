009936482141c6b559b944362bd9c865195d7872
Sanna <santras@users.noreply.github.com>
2020-09-23 13:32:19 +0300

    Laskin

--- DotNet/HelloWorld/Program.cs
+++ DotNet/HelloWorld/Program.cs
@@ -6,7 +6,29 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Herätys Sanna!");
+
+            Console.WriteLine("Anna kokonaisluku:");
+            string kayttajaInput = Console.ReadLine();
+            int ii = Convert.ToInt32(kayttajaInput);
+
+            Console.WriteLine("Anna toinen kokonaisluku:");
+            string kayttajaInput2 = Console.ReadLine();
+            int jj = Convert.ToInt32(kayttajaInput2);
+            
+
+            // Console.WriteLine("Anna kokonaisluku:");
+            // string kayttajaInput2 = Console.ReadLine();
+            // try
+            // {
+            //     int jj = Convert.ToInt32(kayttajaInput2);
+            // }
+            // catch(FormatException)
+            // {
+            //     Console.WriteLine("Luvun pitää olla kokonaisluku, kokeile uudelleen:");   
+            // }
+           
+            Console.WriteLine("Lukujen summma on:"+(ii+jj));
         }
     }
 }
