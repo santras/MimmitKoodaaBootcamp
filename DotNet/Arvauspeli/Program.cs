35566e18d6236198ee569619666a6cfdc21f382c
Sanna <santras@users.noreply.github.com>
2020-09-24 15:45:45 +0300

    Lisätty AspNet testi

---
+++ DotNet/Arvauspeli/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Arvauspeli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tämä on arvauspeli. Mitä on salainen luku?");
+            Random randomi = new Random();
+            int luku  = randomi.Next(1, 20);
+            Boolean oikeinko = false;
+
+            for(int i = 1; i < 4; i++)
+            {
+                string arvaus = Console.ReadLine();
+                int eka = int.Parse(arvaus);
+                if (eka == luku)
+                {
+                    Console.WriteLine("Hurraa!!");
+                    oikeinko = true;
+                    break;
+                }
+                else if (eka<luku)
+                {
+                    Console.WriteLine("Arvauksesi on liian pieni.");
+                }
+                else
+                {
+                    Console.WriteLine("Arvauksesi on liian suuri.");
+                }
+            }
+            if (oikeinko)
+            {
+               Console.WriteLine("Arvasit oikein!"); 
+            }
+            else
+            {
+                Console.WriteLine("Arvasit väärin, luku oli: "+luku); 
+            }
+
+        }
+    }
+}
---
+++ DotNet/Pelihahmot/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Pelihahmot
+{
+
+    class Hahmo
+    {
+        public string Nimi { get; set; }
+        public string Rotu { get; set; }
+        public string Luokka { get; set; }
+        public string Taso { get; set; }
+        public string CPTaso{ get; set; }
+        public string Tyyppi { get; set; }
+        public string Ase { get; set; }
+        public string Haarniska { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
